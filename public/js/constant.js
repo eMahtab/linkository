ff5d205d8147b03f4e8006060a1a44012ab49171
Mahtab Alam <alammahtab08@gmail.com>
2017-11-17 22:10:59 +0530

    Changed API Endpoint URL for deployment

--- public/js/constant.js
+++ public/js/constant.js
@@ -2,5 +2,12 @@
                         .constant('CONSTANT',{"API_URL":"http://www.linkository.com"});
 */
 
+/*  FOR LOCAL DEVELOPMENT  USE THIS  */
+
+/*
 var appConstants=angular.module('app.constants',[])
-                        .constant('CONSTANT',{"API_URL":"http://localhost:8080"});
+                        .constant('CONSTANT',{"API_URL":"http://localhost:8080"});*/
+
+
+var appConstants=angular.module('app.constants',[])
+                        .constant('CONSTANT',{"API_URL":"http://www.linkository.herokuapp.com"});                        
